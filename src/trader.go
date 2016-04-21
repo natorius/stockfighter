c78bfcf4c2876474e391d73d72a0bf95df041cbb
Nate <nate.green@gmail.com>
2016-04-21 13:32:03 +0000

    Add client and authentication

--- src/trader.go
+++ src/trader.go
@@ -12,6 +12,7 @@ type Config struct {
     ApiKey    string
 }
 
+
 func getConfig() Config {
 
 // TODO: figure out how to have global logging var
@@ -27,7 +28,8 @@ func getConfig() Config {
   if err != nil {
     log.Fatal(err)
   }
-  
+
+// TODO: extract json decoding to function  
   decoder := json.NewDecoder(file)
   config  := Config{}
   
@@ -40,21 +42,44 @@ func getConfig() Config {
   return config
 }
 
-func checkURL(urlFrag string) {
-  resp, err := http.Get("https://api.stockfighter.io/ob/api/" + urlFrag)
+type Client struct {
+  Client *http.Client
+  ApiKey string
+}
+
+type Response struct {
+  Ok bool `json:"ok"`
+}
+
+func (c Client) checkURL(urlFrag string) {
+  url      := "https://api.stockfighter.io/ob/api/" + urlFrag
+  req, err := http.NewRequest("GET", url, nil)
+  if err != nil { log.Fatal(err) }
+  req.Header.Add("X-Starfighter-Authorization", c.ApiKey)
+
+  resp, err := c.Client.Do(req)
   if err != nil || resp.StatusCode != 200 { 
     log.Fatal("Error: ", err, "\nResp: ", resp)
   }
   defer resp.Body.Close()
-  log.Println(urlFrag + " UP: ", resp.Body)
+
+  var sfresp Response
+  decoder := json.NewDecoder(resp.Body)
+
+  err      = decoder.Decode(&sfresp)
+  if err != nil { log.Fatal(err) }
+
+  log.Printf("%v UP: %+v", urlFrag, sfresp)
 }
 
 func main() {
   config := getConfig()
   log.Println("key:" + config.ApiKey)
-  //client := &http.Client{}
+  client := Client{&http.Client{}, config.ApiKey}
   
-  checkURL("heartbeat")
-  checkURL("venues/TESTEX/heartbeat")
+  client.checkURL("heartbeat")
+  client.checkURL("venues/TESTEX/heartbeat")
+//  client.checkURL("venues/UKHNEX/heartbeat")
+  
   
 }
